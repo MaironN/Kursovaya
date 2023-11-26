aa5e5f024b1c573190bfbac6c1fc32c292128623
MaironN <96mnastya@mail.ru>
2023-11-26 14:24:27 +0400

    kurs
    kurs

---
+++ src/MultiClubMember.java
@@ -0,0 +1,28 @@
+public class MultiClubMember extends Member {
+
+    private int membershipPoints;
+
+    public MultiClubMember(char memberType, int memberID, String name, double fees, int membershipPoints) {
+        super(memberType, memberID, name, fees);
+        this.membershipPoints = membershipPoints;
+    }
+
+    public int getMembershipPoints() {
+        return membershipPoints;
+    }
+
+    public void setMembershipPoints(int membershipPoints) {
+        this.membershipPoints = membershipPoints;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", "+ membershipPoints;
+    }
+}
+
+   //*public MultiClubMember(char m, int parseInt, String s, double parseDouble, int parseInt1) {
+
+
+
---
+++ src/SingleClubMember.java
@@ -0,0 +1,22 @@
+public class SingleClubMember extends Member {
+    private int club;
+
+    public int getClub() {
+        return club;
+    }
+
+    public void setClub(int club) {
+        this.club = club;
+    }
+
+    public SingleClubMember(char memberType, int memberID, String name, double fees, int club) {
+        super(memberType, memberID, name, fees);
+        this.club = club;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() +
+                ", " + club;
+    }
+}
